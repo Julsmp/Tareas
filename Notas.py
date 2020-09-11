30a5f345526ecdbbb41dac8bea2099681ca3f980
Julsmp <66829194+Julsmp@users.noreply.github.com>
2020-09-11 18:04:22 +0200

    Create Notas.py

---
+++ Notas.py
@@ -0,0 +1,17 @@
+"""
+
+Qué falta?
+
+- Imprimir imprima solo el nombre de la tarea. 
+- Poder acceder a la tarea
+- Dentro de la tarea ver toda la información, descripción, fecha, etc.. 
+- Opciones dentro de la tarea: 
+    - completar tarea 
+    - modificar tarea (poder decidir qué parte modificas)
+- Añadir fecha a la tarea
+- añadir atributo de completado
+- Que la primera tarea del programa sea 'Aprender a utilizar el programa' y aprenda los comandos 
+para evitar que aparezcan siempre las instrucciones y lo podrá recuperar en completados,
+- Ver lista de completados
+
+"""
